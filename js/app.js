69635602ec764544900f15516064b9840579c3b8
Emiliano cabral <emicabral95@gmail.com>
2023-11-27 21:40:39 -0300

    aplicando una funcion

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 // juego de adivinar el numero 
-const numeroSecreto = Math.floor(Math.random() * 10) + 1; 
-let intentos = 0;
-let adivinado = false;
-console.log("¡Bienvenido al juego de adivinar el número!");
-
-do {
-    let player1 = parseInt(prompt("Intenta adivinar el número (del 1 al 10):"));
-    if (isNaN(player1) || player1 < 1 || player1 > 10) {
-        console.log("Por favor, ingresa un número válido del 1 al 10.");
-    } else { intentos++;
-        if (player1 === numeroSecreto) {
-            adivinado = true;
-            console.log(`¡Correcto! Has adivinado el número en ${intentos} intentos.`);
-        } else {
-            console.log("Incorrecto. ¡Inténtalo de nuevo!");
+function juegodeadivinarelnumero () {
+    
+    const numeroSecreto = Math.floor(Math.random() * 10) + 1; 
+    let intentos = 0;
+    let adivinado = false;
+    console.log("¡Bienvenido al juego de adivinar el número!");
+    
+    do {
+        let player1 = parseInt(prompt("Intenta adivinar el número (del 1 al 10):"));
+        if (isNaN(player1) || player1 < 1 || player1 > 10) {
+            console.log("Por favor, ingresa un número válido del 1 al 10.");
+        } else { intentos++;
+            if (player1 === numeroSecreto) {
+                adivinado = true;
+                console.log(`¡Correcto! Has adivinado el número en ${intentos} intentos.`);
+            } else {
+                console.log("Incorrecto. ¡Inténtalo de nuevo!");
+            }
         }
-    }
-} while (!adivinado);
+    } while (!adivinado);
+    
+    console.log("¡Gracias por jugar!");
 
-console.log("¡Gracias por jugar!");
+}
+
+juegodeadivinarelnumero ();
