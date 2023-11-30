761dae6fef9be67a67f8552c84bd911f624339aa
Emiliano cabral <emicabral95@gmail.com>
2023-11-30 20:43:40 -0300

    cambie $

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 // juego de adivinar el numero 
 function juegodeadivinarelnumero () {
-    
     const numeroSecreto = Math.floor(Math.random() * 10) + 1; 
     let intentos = 0;
     let adivinado = false;
     console.log("¡Bienvenido al juego de adivinar el número!");
-    
     do {
         let player1 = parseInt(prompt("Intenta adivinar el número (del 1 al 10):"));
         if (isNaN(player1) || player1 < 1 || player1 > 10) {
@@ -13,7 +11,7 @@ function juegodeadivinarelnumero () {
         } else { intentos++;
             if (player1 === numeroSecreto) {
                 adivinado = true;
-                console.log(`¡Correcto! Has adivinado el número en ${intentos} intentos.`);
+                console.log("Has adivinado el número en " + intentos + " intentos.");
             } else {
                 console.log("Incorrecto. ¡Inténtalo de nuevo!");
             }
@@ -21,7 +19,5 @@ function juegodeadivinarelnumero () {
     } while (!adivinado);
     
     console.log("¡Gracias por jugar!");
-
 }
-
 juegodeadivinarelnumero ();
